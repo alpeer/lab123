743ce66cf880739a88ac0f7c2bdcc4cc4a18f0b6
sysadm <root@ilgilenio.com>
2019-03-07 06:10:51 +0000

    ux

--- alper.js
+++ alper.js
@@ -1,5 +1,5 @@
 denkgeleSayı = ()=> (Math.round(Math.random()*1e8)%10000000)+10
-
+timeout=false;
 document.addEventListener("DOMContentLoaded",()=>{
     let zamanlar = document.querySelectorAll(".zamanlar")
     let sonuçlar = document.querySelectorAll(".sonuçlar")
@@ -8,6 +8,7 @@ document.addEventListener("DOMContentLoaded",()=>{
 	let denkgele = document.querySelectorAll("#denkgele")[0]
 	
     hesapla.onclick=()=>{
+        if(sayıAlanı.value.trim().split('\n').length){
         zamanlar[0].innerHTML=''
         zamanlar[1].innerHTML=''
         sonuçlar[0].innerHTML=''
@@ -24,11 +25,23 @@ document.addEventListener("DOMContentLoaded",()=>{
             ort=z[j].reduce((k, l)=>k+l)/z[j].length
             zamanlar[j].innerHTML+='<br><br>ortalama: '+ort
         })
+        }
     }
 	sayıAlanı.value=''
-	denkgele.onclick=()=>{
+	denkgele.onwheel =function(e){
+	    if(timeout){clearTimeout(timeout)}
+	    e.preventDefault();
+	    e.stopPropagation();
+    	sayı = Number((this.innerHTML).split(" ")[0])
+    	sayı+= e.deltaY>0? -1: +1
+    	sayı = sayı<1?1:sayı
+    	this.innerHTML= sayı +" Denkgele Sayı"
+    	this.onclick()
+    	timeout=setTimeout(()=>hesapla.onclick(),10)
+    }
+	denkgele.onclick=function(e){
 		sayıAlanı.value='';
-       for(let i = 0 ; i<10; i++){
+       for(let i = 0 ; i<Number((this.innerHTML).split(" ")[0]); i++){
 		   sayıAlanı.value+= denkgeleSayı()+" "+ denkgeleSayı()+"\n"
 	   }
     }
